3c4032c5b89daa3d4abf687c0f5e4850506e2ae3
VajdaMate <vajdamate0204@gmail.com>
2025-03-09 21:02:17 +0100

    feat: Changed contract to be able to handle UID-s

--- Hardhat/contracts/BottleStore.sol
+++ Hardhat/contracts/BottleStore.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.24;
 contract BottleStore{
     struct Bottle {
         address owner;
-        uint256 bottleId;
+        uint128 bottleId;
         string typeOfGrape;
         uint32[] sunnyHours;
         uint32[] rainMilimeters;
@@ -13,10 +13,10 @@ contract BottleStore{
     }
     
     Bottle[] public bottles;
-
+    
     
     //Registerhez
-    function registerBottle(string memory _typeOfGrape) public {
+    function registerBottle(uint128 _bottleId, string memory _typeOfGrape) public {
         uint32[] memory tmpSunny = new uint32[](1); 
         tmpSunny[0] = 0;
         uint32[] memory tmpRain = new uint32[](1);
@@ -26,26 +26,30 @@ contract BottleStore{
         string[] memory tmpTimeOfBottling = new string[](1);
         tmpTimeOfBottling[0] = "M\u00e9g nem palackozott";
 
-        bottles.push(Bottle(msg.sender,bottles.length,_typeOfGrape,tmpSunny,tmpRain,tmpTimeOfHarvest,tmpTimeOfBottling));
+        bottles.push(Bottle(msg.sender,_bottleId,_typeOfGrape,tmpSunny,tmpRain,tmpTimeOfHarvest,tmpTimeOfBottling));
     }
 
     function registerBottle(
-        string memory _typeOfGrape, uint32[] memory _sunnyHours, uint32[] memory _rainMilimiters, string[] memory _timeOfHarvest, string[] memory _timeOfBottling) public 
+        uint128 _bottleId, string memory _typeOfGrape, 
+        uint32[] memory _sunnyHours, uint32[] memory _rainMilimiters, 
+        string[] memory _timeOfHarvest, string[] memory _timeOfBottling) public 
     {
-        bottles.push(Bottle(msg.sender,bottles.length,_typeOfGrape,_sunnyHours,_rainMilimiters,_timeOfHarvest,_timeOfBottling));
+        bottles.push(Bottle(msg.sender,_bottleId,_typeOfGrape,_sunnyHours,_rainMilimiters,_timeOfHarvest,_timeOfBottling));
     }
 
 
-    function returnLastBottleID()public view returns (uint256){
+    function returnLastBottleID()public view returns (uint128){
         return bottles[bottles.length-1].bottleId;
     }
-
-    
     
     
     //Checkhez, updatehez
     function returnBottleByID(uint256 _id)public view returns (Bottle memory){
-        return bottles[_id];
+        for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+               return bottles[i];
+            }
+        }
     }
 
     //Checkhez
@@ -72,33 +76,70 @@ contract BottleStore{
     //Updatehez
 
     function updateSunnyHours(uint256 _id, uint32 _sunnyHours) public {
-        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!"); //Unicode karakterek
+         uint128 tempId;
+         for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+                tempId=i;
+                break;
+            }
+        }
+        require(bottles[tempId].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!"); //Unicode karakterek
         
-        bottles[_id].sunnyHours.push(_sunnyHours);
+        bottles[tempId].sunnyHours.push(_sunnyHours);
     }
 
     function updateRainMilimeters(uint256 _id, uint32 _rainMilimeters) public {
-        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
+        uint128 tempId;
+         for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+                tempId=i;
+                break;
+            }
+        }
+        
+        require(bottles[tempId].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
        
-        bottles[_id].rainMilimeters.push(_rainMilimeters);
+        bottles[tempId].rainMilimeters.push(_rainMilimeters);
     }
 
     function updateTimeOfHarvest(uint256 _id, string memory _timeOfHarvest) public {
-        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
-        bottles[_id].timeOfHarvest.push(_timeOfHarvest);
+        uint128 tempId;
+         for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+                tempId=i;
+                break;
+            }
+        }
+        require(bottles[tempId].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
+        bottles[tempId].timeOfHarvest.push(_timeOfHarvest);
     }
 
     function updateTimeOfBottling(uint256 _id, string memory _timeOfBottling) public {
-        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
+        uint128 tempId;
+         for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+                tempId=i;
+                break;
+            }
+        }
+        require(bottles[tempId].owner == msg.sender, "Csak saj\u00E1t \u00FCveget friss\u00EDthetsz!");
       
-        bottles[_id].timeOfBottling.push(_timeOfBottling);
+        bottles[tempId].timeOfBottling.push(_timeOfBottling);
     }
 
 
     //Tulajváltás
     function giveBottle(uint256 _id, address _newOwner) public {
-        require(bottles[_id].owner == msg.sender, "Csak saj\u00E1t \u00FCveget adhatsz!");
-        bottles[_id].owner = _newOwner;
+        uint128 tempId;
+         for (uint128 i = 0; i < bottles.length; i++) {
+            if (bottles[i].bottleId == _id) {
+                tempId=i;
+                break;
+            }
+        }
+        
+        require(bottles[tempId].owner == msg.sender, "Csak saj\u00E1t \u00FCveget adhatsz!");
+        bottles[tempId].owner = _newOwner;
     }
 
 
